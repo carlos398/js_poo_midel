694a316d1426375b97043874ae41b7328ee007ee
Carlos Andres Reyes Peña <carlosarp2000@gmail.com>
2022-02-18 08:41:29 -0500

    manejo de recursividad

---
+++ main.js
@@ -0,0 +1,19 @@
+//Recursividad
+
+const numeritos = [0,1,2,3,4,5,5,6,7,8,9,435678,7,2,3];
+// let numerito = 0;
+// for (let index = 0; index < numeritos.length; index++) {
+//   numerito = numeritos[index];
+//   console.log({ index, numerito });
+// }
+
+function recursiva(numbersArray) {
+  if (numbersArray.length != 0) {
+    const firstNum = numbersArray[0];
+    console.log(firstNum);
+
+    numbersArray.shift();
+    recursiva(numbersArray);
+  }
+}
+
